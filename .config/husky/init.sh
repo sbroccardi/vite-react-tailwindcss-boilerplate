c0ad485123842a428eee524a54b0c05ba3633feb
Sergio Broccardi <sbroccardi@gmail.com>
2024-04-24 22:39:43 -0300

    feat: husky added

--- .config/husky/init.sh
+++
@@ -1,7 +0,0 @@
-# ~/.config/husky/init.sh
-# . ~/.zshrc
-#export NVM_DIR=~/.nvm
-#source $(brew --prefix nvm)/nvm.sh # This loads nvm
-#export PATH=$PATH:~/.yarn/bin # This add Yarn
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-exit 1 # test Hook Without Committing
